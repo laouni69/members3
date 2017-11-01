12460d4f0493cf4728d4b88acefe1f90b1ae9d48
laouni69 <youssef.benghalem@gmail.com>
2017-11-01 18:24:18 +0100

    final-login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031093454) do
+ActiveRecord::Schema.define(version: 20171101142757) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
